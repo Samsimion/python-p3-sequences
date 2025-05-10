ebc723b97d2fba9a390ff52c59c55ea2b5f3ca17
simion samuel <samuel.simion@student.moringaschool.com>
2025-05-10 15:20:24 +0300

    assign

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    for i in range(length):
+        if i == 0:
+            my_list.append(0)
+
+        elif i == 1:
+            my_list.append(1)
+        else:
+            my_list.append(my_list[i-1]+my_list[i-2])
+
+    print(my_list)
+
+
+print_fibonacci(20)
+        
+    
+    
